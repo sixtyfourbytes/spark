4862b0ab7268ca657b06126c7917225e29ff27ef
sixtyfourbytes <sixty4bytes@gmail.com>
2026-09-14 04:23:48 +0400

    add array_agg example to friendsByAgeDF

--- friendsByAgeDF.py
+++ friendsByAgeDF.py
@@ -1,15 +1,25 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as funcs
 
-spark = SparkSession.builder.appName('Friends By Age Ex Using DataFrame').getOrCreate()
+spark = SparkSession.builder.appName("Friends By Age Ex Using DataFrame").getOrCreate()
 
-people = spark.read.option('header', 'true').option('inferSchema', 'true').csv('./DataFiles/fakefriends-header.csv')
+people = (
+    spark.read.option("header", "true")
+    .option("inferSchema", "true")
+    .csv("./DataFiles/fakefriends-header.csv")
+)
 
-friendsByAge = people.select('age', 'friends')
+friendsByAge = people.select("age", "friends")
 
-friendsByAge.groupBy('age').avg('friends').sort('age').show()
+friendsByAge.groupBy("age").avg("friends").sort("age").show()
 
-friendsByAge.groupBy('age').agg(funcs.round(funcs.avg('friends'), 2).alias('friendsAvg')).sort('age').show()
+# friendsByAge.groupBy("age").agg(
+#         funcs.array_agg('friends')
+# ).sort("age").show()
+
+friendsByAge.groupBy("age").agg(
+    funcs.round(funcs.avg("friends"), 2).alias("friendsAvg")
+).sort("age").show()
 
 people.show(20)
 
