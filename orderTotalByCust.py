6e65337fa6f0af9c29f31b304a14e025853863f2
sixtyfourbytes <sixty4bytes@gmail.com>
2026-09-14 04:50:36 +0400

    format code across scirpts

--- orderTotalByCust.py
+++ orderTotalByCust.py
@@ -1,12 +1,14 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster('local').setAppName('OrderTotalByCustomer')
+conf = SparkConf().setMaster("local").setAppName("OrderTotalByCustomer")
 sc = SparkContext(conf=conf)
 
-lines = sc.textFile('./DataFiles/customer-orders.csv')
-custSpent = lines.map(lambda x: (x.split(',')[0], float(x.split(',')[2])))
-custSpent = (custSpent.reduceByKey(lambda x, y: x + y)
-             .map(lambda x: (round(x[1], 2), x[0]))
-             .sortByKey())
+lines = sc.textFile("./DataFiles/customer-orders.csv")
+custSpent = lines.map(lambda x: (x.split(",")[0], float(x.split(",")[2])))
+custSpent = (
+    custSpent.reduceByKey(lambda x, y: x + y)
+    .map(lambda x: (round(x[1], 2), x[0]))
+    .sortByKey()
+)
 
 print(custSpent.collect())
--- wordCount.py
+++ wordCount.py
@@ -2,13 +2,15 @@ import re
 
 from pyspark import SparkConf, SparkContext
 
+
 def normalizeWord(text):
-    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+    return re.compile(r"\W+", re.UNICODE).split(text.lower())
 
-conf = SparkConf().setMaster('local').setAppName('WordCount')
-sc = SparkContext(conf = conf)
 
-lines = sc.textFile('./DataFiles/book.txt')
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf=conf)
+
+lines = sc.textFile("./DataFiles/book.txt")
 words = lines.flatMap(normalizeWord)
 wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
 wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
@@ -16,6 +18,6 @@ results = wordCountsSorted.collect()
 
 for result in results:
     count = str(result[0])
-    cleanWord = result[1].encode('ascii', 'ignore')
+    cleanWord = result[1].encode("ascii", "ignore")
     if cleanWord:
         print(cleanWord, count)
